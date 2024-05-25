b6fe198af25910228f0196dae8f235b5b6c57af3
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2024-05-25 07:24:36 +0300

    add emoji at book annotation

--- src/helpers/regex/book.regexp.ts
+++ src/helpers/regex/book.regexp.ts
@@ -1,4 +1,4 @@
 export const bookRegExp =
-  /^[a-zA-Zа-яА-ЯіІїЇєЄґҐ\d\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+$/;
+  /^[a-zA-Zа-яА-ЯіІїЇєЄґҐ\d\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\p{Emoji}]+$/u;
 
 export const bookMessage = 'Field must contain only Cyrillic or Latin letters';
